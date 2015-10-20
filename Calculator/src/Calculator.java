c0aa4395886b66e67cf80905f368f8e7a0fa9c89
DAVIDW59772 <dwilkins.59772@farnborough.ac.uk>
2015-10-20 12:14:02 +0100

    Commit Three
    Committed 12:13 20/10/15

--- Calculator/src/Calculator.java
+++ Calculator/src/Calculator.java
@@ -6,6 +6,38 @@
  * @author CompUser
  *
  */
-public class Calculator {
 
+public class Calculator {
+	private float numberOne;
+	private float numberTwo;
+
+
+	public Calculator(){}
+
+	public float getNumberOne(){
+		return this.numberOne;
+	}
+	public void setNumberOne(float n1){
+		this.numberOne = n1;
+	}
+	public float getNumberTwo(){
+		return this.numberTwo;
+	}
+	public void setNumberTwo(float n2){
+		this.numberTwo = n2;
+	}
+	
+	public float add(){
+		return this.numberOne + numberTwo;
+	}
+	public float subtract(){
+		return this.numberOne - numberTwo;
+	}
+	public float multiply(){
+		return this.numberOne * numberTwo;
+	}
+	public float divide(){
+		return this.numberOne / numberTwo;
+	}
+	
 }
